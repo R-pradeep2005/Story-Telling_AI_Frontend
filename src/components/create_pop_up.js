c03bf726f654b6b05572bb4c2f992797b5cb28e5
R-pradeep2005 <web.dev.roar@gmail.com>
2024-09-04 20:26:41 +0530

    setup

---
+++ src/components/create_pop_up.js
@@ -0,0 +1,9 @@
+
+const Create_pop_up=()=>{
+    return(
+        <div>
+            
+        </div>
+    )
+}
+export default Create_pop_up;
---
+++ src/screens/sign_in.js
@@ -0,0 +1,8 @@
+const Sign_in=()=>{
+    return(
+        <div>
+            
+        </div>
+    )
+}
+export default Sign_in;
